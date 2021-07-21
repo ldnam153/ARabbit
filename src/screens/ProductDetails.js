793ae6af218b93eda6b367e8b0d2abc889310ec2
lethanhson1504 <51450607+lethanhson1504@users.noreply.github.com>
2021-07-21 18:13:54 +0700

    Commit Slide Image of product details screen

---
+++ src/screens/ProductDetails.js
@@ -0,0 +1,76 @@
+import React, {Component} from 'react';
+import {StyleSheet, View, Text, Dimensions, Image} from 'react-native';
+import {Icon} from 'react-native-elements';
+import ViewSlider from 'react-native-view-slider';
+
+const {width, height} = Dimensions.get('window');
+
+class ProductDetails extends Component {
+  render() {
+    return (
+      <>
+        <ViewSlider
+          renderSlides={
+            <>
+              <View style={styles.viewBox}>
+                <Image
+                  source={{
+                    uri: 'https://image.shutterstock.com/image-photo/beautiful-autumn-scene-hintersee-lake-260nw-747646759.jpg',
+                  }}
+                  style={{height: 200, width}}
+                />
+              </View>
+              <View style={styles.viewBox}>
+                <Text>TWO</Text>
+              </View>
+              <View style={styles.viewBox}>
+                <Text>THREE</Text>
+              </View>
+              <View style={styles.viewBox}>
+                <Text>FOUR</Text>
+              </View>
+            </>
+          }
+          style={styles.slider} //Main slider container style
+          height={200} //Height of your slider
+          slideCount={4} //How many views you are adding to slide
+          dots={true} // Pagination dots visibility true for visibile
+          dotActiveColor="red" //Pagination dot active color
+          dotInactiveColor="gray" // Pagination do inactive color
+          dotsContainerStyle={styles.dotContainer} // Container style of the pagination dots
+          //autoSlide={true} //The views will slide automatically
+          //slideInterval={1000} //In Miliseconds
+        />
+        <Text style={styles.textStyle}>Hello son!</Text>
+      </>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  viewBox: {
+    paddingHorizontal: 20,
+    justifyContent: 'center',
+    width: width,
+    padding: 10,
+    alignItems: 'center',
+    height: 150,
+  },
+  slider: {
+    alignSelf: 'center',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'pink',
+  },
+  dotContainer: {
+    backgroundColor: 'transparent',
+    position: 'absolute',
+    bottom: 15,
+  },
+  textStyle: {
+    color: '#ffff',
+    fontSize: 23,
+  },
+});
+
+export default ProductDetails;
