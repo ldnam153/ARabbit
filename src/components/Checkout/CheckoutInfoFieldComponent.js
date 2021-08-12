56c1dc9883fa73023754a9ba98ccfdf1f4efd84b
anh_duy_dep_trai <nguyenanhduy0400@gmail.com>
2021-08-12 22:45:20 +0700

    [New] Add new address feature

--- src/components/Checkout/CheckoutInfoFieldComponent.js
+++ src/components/Checkout/CheckoutInfoFieldComponent.js
@@ -1,15 +1,76 @@
 import React, { Component } from 'react';
-import { View, Text, StyleSheet, Image } from 'react-native';
+import { View, Text, StyleSheet, TextInput } from 'react-native';
+import SelectDropdown from 'react-native-select-dropdown';
 
 class CheckoutInfoFieldComponent extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      dataProvince: [],
+      dataDistrict: [],
+      dataWard: [],
+    };
+    this.onChange = this.onChange.bind(this);
+  }
+  componentDidMount() {
+    this.props.index === 2 &&
+      fetch('https://provinces.open-api.vn/api/p/')
+        .then((res) => res.json())
+        .then(
+          (result) => {
+            this.setState({
+              dataProvince: result,
+            });
+          },
+          (error) => {
+            alert('Fetching error: ' + error);
+          }
+        );
+  }
+  componentDidUpdate(prevProps) {
+    prevProps.districts !== this.props.districts && this.setState({dataDistrict: this.props.districts})
+    prevProps.wards !== this.props.wards && this.setState({dataWard: this.props.wards})
+  }
+  onChange = (text) => {
+    this.props.index === 0 && this.props.changeName(text);
+    this.props.index === 1 && this.props.changePhone(text);
+    this.props.index === 5 && this.props.changeAddress(text);
+  }
   render() {
     return (
       <View style={this.props.last ? styles.last_item : styles.container}>
-        <Text style={styles.field_title}>{this.props.title}</Text>
-        <Text style={styles.field_value}>
-          {this.props.value}{' '}
-          {this.props.forward && <Image source={require('../../resources/icons/forward.png')} />}
-        </Text>
+        {this.props.type === 'input' ? (
+          <View
+            style={[
+              { width: '100%', display: 'flex', flexDirection: 'row' },
+              this.props.last && { flexDirection: 'column', alignItems: 'flex-start' },
+            ]}
+          >
+            <Text style={styles.field_title}>{this.props.title}</Text>
+            <TextInput style={styles.input} placeholder={this.props.value} onChangeText={(text) => this.onChange(text)}/>
+          </View>
+        ) : (
+          <View style={{ display: 'flex', flexDirection: 'row' }}>
+            <Text style={styles.field_title}>{this.props.title}</Text>
+            <SelectDropdown
+              data={this.props.index === 2 ? this.state.dataProvince.map(item => item.name) : this.props.index === 3 ? this.state.dataDistrict.map(item => item.name) : this.props.index === 4 && this.state.dataWard.map(item => item.name)}
+              onSelect={(selectedItem, index) => {
+                this.props.index === 2 && this.props.changeProvince(this.state.dataProvince[index]);
+                this.props.index === 3 && this.props.changeDistrict(this.state.dataDistrict[index]);
+                this.props.index === 4 && this.props.changeWard(this.state.dataWard[index]);
+              }}
+              defaultButtonText={'Chọn'}
+              buttonTextAfterSelection={(selectedItem, index) => {
+                return selectedItem;
+              }}
+              rowTextForSelection={(item, index) => {
+                return item;
+              }}
+              buttonStyle={styles.dropdown2BtnStyle}
+              buttonTextStyle={styles.dropdown2BtnTxtStyle}
+            />
+          </View>
+        )}
       </View>
     );
   }
@@ -32,13 +93,13 @@ const styles = StyleSheet.create({
     fontSize: 16,
     fontWeight: 'bold',
     flex: 1,
-    textAlign: 'left'
+    textAlign: 'left',
   },
 
   field_value: {
     fontSize: 14,
     flex: 1,
-    textAlign: 'right'
+    textAlign: 'right',
   },
 
   forward: {
@@ -56,6 +117,34 @@ const styles = StyleSheet.create({
     borderBottomWidth: 1,
     paddingHorizontal: 20,
     paddingVertical: 15,
+  },
+
+  dropdown2BtnStyle: {
+    width: '110%',
+    height: 30,
+    backgroundColor: '#e3dede',
+    borderRadius: 8,
+    flex: 1,
+  },
+
+  dropdown2BtnTxtStyle: {
+    color: '#000',
+    textAlign: 'left',
+    fontSize: 14,
+    flex: 1,
+  },
+
+  input: {
+    fontSize: 14,
+    flex: 1,
+    textAlign: 'left',
+    paddingVertical: 0,
+    borderTopWidth: 0,
+    borderLeftWidth: 0,
+    borderRightWidth: 0,
+    borderBottomWidth: 1,
+    borderBottomColor: '#d3d3d3',
+    width: '100%',
   },
 });
 
